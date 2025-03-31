6f1f8dac0f3678bea3d89a0c04c382c62077ea47
bstocker <boris.stocker@gmail.com>
2025-03-31 14:21:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def exercice(valeur):
     etoiles = ''
     for j in range(valeur):
-        for i in range(valeur):
+        for i in range(i+1):
             etoiles += '*'
         etoiles += '<br>'
     return etoiles
