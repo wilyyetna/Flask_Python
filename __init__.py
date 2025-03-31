fd38f3b0695a378a4f714030eb274582370be307
wilyyetna <william.yetna@ecoles-epsi.net>
2025-03-31 12:29:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
-#comment
 
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    etoiles = ''
-    for i in range(valeur):
-        etoiles += '*'
-    return etoiles
-#comment
-
-if __name__ == "__main__":
-  app.run(debug=True)
-Copier/Coller ce code dans votre fichier __init__.py (remplacer votre code d'origine)
-```PYTHON
 from flask import Flask
 from flask import render_template
 from flask import json                                                                                                                                     
