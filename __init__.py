605df9265ef06b24e8a61864640395b39dc2c321
wilyyetna <william.yetna@ecoles-epsi.net>
2025-04-07 12:28:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,28 +1,22 @@
 from flask import Flask
+from flask import render_template
+from flask import json
+
 
 app = Flask(name)
 
-Fonction pour générer la suite de Fibonacci jusqu'à n
-def fibonacci(n):
-    fib = [0, 1]  # Initialiser la suite avec les deux premiers termes
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])  # Ajouter un terme en suivant la relation Un = Un-1 + Un-2
-    return fib
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    somme = 0
+    for i in range(1, valeur + 1):
+        if i % 11 == 0:
+            continue
+        if i % 5 == 0 or i % 7 == 0:
+            if somme + i > 5000:
+                break
+            somme += i
 
-@app.route('/<int:n>')
-def afficher_fibonacci(n):
-    # Si n est 1, on renvoie juste [0]
-    if n == 1:
-        fib_sequence = [0]
-    else:
-        # Générer la suite de Fibonacci jusqu'à n termes
-        fib_sequence = fibonacci(n)
-
-Convertir la suite en chaîne pour l'affichage
-    result = ', '.join(map(str, fib_sequence))
-
-Retourner le résultat formaté
-    return f"<h1>Suite de Fibonacci jusqu'au {n}-ième terme :</h1><p>{result}</p>"
+    return f'<pre>Somme finale: {somme}</pre>'
 
 if name == "main":
     app.run(debug=True)
