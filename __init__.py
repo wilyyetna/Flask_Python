0e26c2852c9a387ff191139ce707bf944f1b1b6f
wilyyetna <william.yetna@ecoles-epsi.net>
2025-04-07 14:54:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
 
-if __name__ == '__main__':
+    # Trouver le maximum et son indice sans utiliser max()
+    max_val = liste_nombres[0]
+    max_index = 0
+
+    for i in range(1, len(liste_nombres)):
+        if liste_nombres[i] > max_val:
+            max_val = liste_nombres[i]
+            max_index = i
+
+    return f"Le plus grand nombre est : {max_val}, à l'indice : {max_index}"
+
+if name == 'main':
     app.run(host='0.0.0.0')
-
